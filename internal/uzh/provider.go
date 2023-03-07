lukasmoellerch/mensa-cli

commit 2f24fef70c70cc2af4e7f4e2be9ba426857dafe6

Remove debug output

internal/uzh/provider.go
@@ -190,10 +190,6 @@ func (p *Provider) FetchCanteens(ctx context.Context, lang string) ([]base.Cante
 	}
 
 	result := make([]base.CanteenMetadata, 0, len(canteenMap))
-	for _, meta := range canteenMap {
-		result = append(result, meta)
-		fmt.Printf("%#v\n", meta)
-	}
 	return result, nil
 }
 

commit c0368f8e216118b2d89373e7f91e8d616bef60d9

Fix support for UZH mensas
This fixes support for UZH canteens which seems to have been broken by commit 2f24fef70c70cc2af4e7f4e2be9ba426857dafe6.

Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

internal/uzh/provider.go
@@ -190,6 +190,10 @@ func (p *Provider) FetchCanteens(ctx context.Context, lang string) ([]base.Cante
 	}
 
 	result := make([]base.CanteenMetadata, 0, len(canteenMap))
+	for _, meta := range canteenMap {
+		result = append(result, meta)
+	}
+
 	return result, nil
 }
 